10d06d540c461781b40e3c9a2a1023ff0f664b90
uday97 <uday.agg97@gmail.com>
2020-06-07 19:50:02 +0530

    added comments and tagging functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,22 @@
 class Article < ApplicationRecord
+	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings, dependent: :destroy
+
+	def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(', ')
+  end
+
+  def tag_list=(tag_string)
+    tags = tag_string.split(',').collect do |tag|
+      tag.strip.downcase
+    end
+    tags = tags.uniq
+    tag_objects = tags.collect do |tag|
+      Tag.find_or_create_by(name: tag)
+    end
+    self.tags = tag_objects
+  end
 end
